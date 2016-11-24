a28a833e386d991be2cab5fc2145a45c976a2921
gksquf1781 <gksquf1781@gmail.com>
2016-11-24 17:05:04 +0900

    ExArray class implement, practice Template

--- 11-17/11-17/thread.cpp
+++ 11-17/11-17/thread.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include "KeyEvent.h"
 #include "Frame.h"
+#include "ExArray.h"
 using namespace std;
 /*unsigned int __stdcall mythread(void*)
 {
@@ -103,7 +104,7 @@ int main()
 	return 0;
 }*/
 
-void main() {
+/*void main() {
 	try {
 		Frame *f1 = new Frame;
 		Frame *f2 = new Frame[10];
@@ -116,4 +117,37 @@ void main() {
 		cout << "Error msg :" << msg << endl;
 	}
 	while (1);
+}*/
+
+void main() {
+	ExArray<int> arr1(3);
+	ExArray<char> arr2(4);
+	ExArray<double> arr3(5);
+
+	arr1.addData(2);
+	arr2.addData('A');
+	arr3.addData(32.12);
+
+	arr1.printData();
+	arr2.printData();
+	arr3.printData();
+
+	int num1 = 10, num2 = 40;
+	cout << "before : " << num1 << ", " << num2 << endl;
+	mySwap<int>(num1, num2);
+	cout << "after : " << num1 << ", " << num2 << endl;
+
+	double num1d = 121.11, num2d = 10.5;
+	cout << "before : " << num1d << ", " << num2d << endl;
+	mySwap<double>(num1d, num2d);
+	cout << "after : " << num1d << ", " << num2d << endl;
+
+	ExArray<int> arr4(5);
+	arr4.addData(10);
+
+	mySwap<ExArray<int>>(arr1, arr4);
+	arr1.printData();
+	arr4.printData();
+	while (1);
+
 }
